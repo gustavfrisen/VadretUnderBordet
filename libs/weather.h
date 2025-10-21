9c98c61fc23b03e2f4844b4e46413973d9ceb58d
Gustav Frisén <gustav.frisen@outlook.com>
2025-10-21 22:54:48 +0200

    Made weather_t struct into a typedef struct and removed getter functions.

--- libs/weather.h
+++ libs/weather.h
@@ -4,7 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct weather_t {
+typedef struct {
     double latitude;
     double longitude;
     double generationtime_ms;
@@ -27,7 +27,7 @@ struct weather_t {
     int winddirection;
     int is_day;
     int weathercode;
-}
+} weather_t;
 
 // Basic functions
 int write_weather_to_file(const weather_t* w, const char* filename);
@@ -35,29 +35,5 @@ int load_weather_from_file(weather_t* w, const char* filename);
 int string_to_weather(const weather_t* w, char* buffer, size_t bufferSize);
 int weather_to_string(const char* data, weather_t* w);
 int weather_print(const weather_t* w);
-
-// Getter functions
-double get_latitude(const weather_t* w);
-double get_longitude(const weather_t* w);
-double get_generation_time(const weather_t* w);
-int get_utc_offset(const weather_t* w);
-const char* get_timezone(const weather_t* w);
-const char* get_timezone_abbreviation(const weather_t* w);
-double get_elevation(const weather_t* w);
-
-const char* get_unit_time(const weather_t* w);
-const char* get_unit_interval(const weather_t* w);
-const char* get_unit_windspeed(const weather_t* w);
-const char* get_unit_winddirection(const weather_t* w);
-const char* get_unit_is_day(const weather_t* w);
-const char* get_unit_weathercode(const weather_t* w);
-
-const char* get_time(const weather_t* w);
-int get_interval(const weather_t* w);
-double get_temperature(const weather_t* w);
-double get_windspeed(const weather_t* w);
-int get_winddirection(const weather_t* w);
-int get_is_day(const weather_t* w);
-int get_weathercode(const weather_t* w);
 
 #endif // WEATHER_H
