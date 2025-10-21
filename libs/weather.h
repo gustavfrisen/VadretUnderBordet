9d51ed6f42758b5c89a83d220e18b61479c4bc79
Gustav Frisén <gustav.frisen@outlook.com>
2025-10-21 16:58:35 +0200

    Added getter functions.

--- libs/weather.h
+++ libs/weather.h
@@ -29,10 +29,35 @@ struct weather_t {
     int weathercode;
 }
 
+// Basic functions
 int write_weather_to_file(const weather_t* w, const char* filename);
 int load_weather_from_file(weather_t* w, const char* filename);
-int weather_to_string(const char* data, weather_t* w);
 int parse_weather_string(const weather_t* w, char* buffer, size_t bufferSize);
+int weather_to_string(const char* data, weather_t* w);
 int weather_print(const weather_t* w);
+
+// Getter functions
+double get_latitude(const weather_t* w);
+double get_longitude(const weather_t* w);
+double get_generation_time(const weather_t* w);
+int get_utc_offset(const weather_t* w);
+const char* get_timezone(const weather_t* w);
+const char* get_timezone_abbreviation(const weather_t* w);
+double get_elevation(const weather_t* w);
+
+const char* get_unit_time(const weather_t* w);
+const char* get_unit_interval(const weather_t* w);
+const char* get_unit_windspeed(const weather_t* w);
+const char* get_unit_winddirection(const weather_t* w);
+const char* get_unit_is_day(const weather_t* w);
+const char* get_unit_weathercode(const weather_t* w);
+
+const char* get_time(const weather_t* w);
+int get_interval(const weather_t* w);
+double get_temperature(const weather_t* w);
+double get_windspeed(const weather_t* w);
+int get_winddirection(const weather_t* w);
+int get_is_day(const weather_t* w);
+int get_weathercode(const weather_t* w);
 
 #endif // WEATHER_H
